5577e148980f7e64a81c31c75be39e8216ce46e8
ERP-Home <xjjmbx@gmail.com>
2025-07-11 14:45:49 +0000

    deactivated the "enable SSS" in boot.py and hooks.py

--- custom_logic/hooks.py
+++ custom_logic/hooks.py
@@ -10,11 +10,8 @@ app_description = "Server Scripts Aktivierung"
 app_version = "1.0.0"
 
 # Boot Session - Server Scripts aktivieren
-boot_session = "custom_logic.boot.enable_server_scripts"
+# boot_session = "custom_logic.boot.enable_server_scripts"
 # Forward / to /app
 website_route_rules = [
     {"from_route": "/", "to_route": "/app"}
 ]
-
-# Boot Session - wird bei jedem Login ausgeführt
-boot_session = "custom_logic.boot.enable_server_scripts"
